aeb4128a0011092d8cde1b8d7704de5da928f268
Luan Souza <luanscudeler@gmail.com>
2022-08-15 15:59:23 +0100

    improve AppIcon component

--- src/components/AppIcon.vue
+++ src/components/AppIcon.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
-defineProps(['size', 'icon'])
+defineProps(['size', 'icon', 'title'])
 </script>
 
 <template>
-  <component :is="icon" :width="size" :height="size"></component>
+  <component :is="icon" :width="size" :height="size">
+    <title>{{ title }}</title>
+  </component>
 </template>
